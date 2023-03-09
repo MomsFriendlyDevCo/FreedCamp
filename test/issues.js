b284b7b52d2be271259feab8d635f8c9cb5982cf
Matt Carter <m@ttcarter.com>
2023-03-09 13:14:07 +1000

    test: Post test fixes

--- test/issues.js
+++ test/issues.js
@@ -23,6 +23,10 @@ describe('FeedCamp.Issues', function() {
 		fcIssues = new FCIssues({auth: fcAuth});
 	});
 
+	before('clear cache', ()=>
+		fcIssues.cache.clear()
+	);
+
 	let issues;
 	it('fetch all issues', ()=> Promise.resolve()
 		.then(()=> fcIssues.fetchAll())
@@ -33,20 +37,43 @@ describe('FeedCamp.Issues', function() {
 		})
 	);
 
+	it('issues conform to FCIssue spec', ()=> {
+		issues.forEach(issue => {
+			expect(issue).to.be.an('object');
+			expect(issue).to.have.property('id');
+			expect(issue.id).to.be.a('string');
+			expect(issue).to.have.property('ref');
+			expect(issue.ref).to.be.a('string');
+			expect(issue.ref).to.match(/\d{4,}$/);
+			expect(issue).to.have.property('title');
+			expect(issue.title).to.be.a('string');
+			expect(issue).to.have.property('assignee');
+			expect(issue.assignee).to.be.a('string');
+			expect(issue).to.have.property('status');
+			expect(issue.status).to.be.a('string');
+			expect(issue).to.have.property('url');
+			expect(issue.url).to.be.a('string');
+			expect(issue.url).to.match(/^https:\/\/freedcamp.com\/view\/\d+\/issuetracker\/\d+\/?$/);
+			expect(issue).to.have.property('raw');
+			expect(issue.raw).to.be.an('object');
+		});
+	});
+
 	it('retrieve random issues', ()=> Promise.resolve()
 		.then(()=> Promise.all([
-			fcIssues.get(issues[100].ref),
-			fcIssues.get(issues[200].ref),
-			fcIssues.get(issues[200].ref),
+			fcIssues.get(issues[1].ref),
+			fcIssues.get(issues[5].ref),
+			fcIssues.get(issues[10].ref),
 		]))
-		.then((...issues) => {
+		.then(issues => {
 			issues.forEach(i => {
 				expect(i).to.have.property('id');
+				expect(i).to.have.property('ref');
 			});
 		})
 	)
 
-	it.only('should retrieve one issue after cache purge', ()=> Promise.resolve()
+	it('should retrieve one issue after cache purge', ()=> Promise.resolve()
 		.then(()=> fcIssues.cache.clear())
 		.then(()=> fcIssues.get(testIssueId))
 		.then(res => {
@@ -57,14 +84,14 @@ describe('FeedCamp.Issues', function() {
 	it('should retrieve random issues after cache purge', ()=> Promise.resolve()
 		.then(()=> fcIssues.cache.clear())
 		.then(()=> Promise.all([
-			fcIssues.get(issues[100].ref),
-			fcIssues.get(issues[200].ref),
-			fcIssues.get(issues[200].ref),
+			fcIssues.get(issues[10].ref),
+			fcIssues.get(issues[20].ref),
+			fcIssues.get(issues[30].ref),
 		]))
-		.then((...res) => {
-			expect(res[0]).to.deep.equal(issues[100]);
-			expect(res[1]).to.deep.equal(issues[200]);
-			expect(res[2]).to.deep.equal(issues[300]);
+		.then(res => {
+			expect(res[0]).to.deep.equal(issues[10]);
+			expect(res[1]).to.deep.equal(issues[20]);
+			expect(res[2]).to.deep.equal(issues[30]);
 		})
 	);
 
